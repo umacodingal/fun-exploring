6c3c625f6c683674985478bec60cb1805cd9310d
umacodingal <uma.codingal@gmail.com>
2024-09-15 06:55:29 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,8 @@
+public class rectangle {
+    public static void main(String args[]) {
+        int width = 5;
+        int height = 10;
+        int area = width * height;
+        System.out.println("Area of rectangle=" + area);
+    }
+}
