44d114eb9e1019655dea0485c1294712a4093286
umacodingal <uma.codingal@gmail.com>
2024-09-15 06:53:28 +0530

    Add files via upload

---
+++ encapsulation.java
@@ -0,0 +1,17 @@
+//It has a private data member and getter and setter methods.  
+
+
+public class encapsulation {
+    // private data member
+    private String name;
+
+    // getter method for name
+    public String getName() {
+        return name;
+    }
+
+    // setter method for name
+public void setName(String name){  
+this.name=name  
+}
+}
