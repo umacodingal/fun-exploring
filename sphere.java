4d161500fc4b8503d612c09841104d9084660a54
umacodingal <uma.codingal@gmail.com>
2024-09-15 07:18:20 +0530

    Add files via upload

---
+++ sphere.java
@@ -0,0 +1,8 @@
+public class sphere {
+    public static void main(String args[]) {
+        int radius = 48;
+        double pie = 3.14285714286;
+        double volume = (4.0 / 3.0) * pie * (radius * radius * radius);
+        System.out.println("Volume of the sphere=" + volume);
+    }
+}
