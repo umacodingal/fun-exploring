1753075d5160f56fa35d6965b5402136354b14ed
umacodingal <uma.codingal@gmail.com>
2024-09-15 06:18:41 +0530

    Add files via upload

---
+++ super.java
@@ -0,0 +1,19 @@
+class Super {
+    String color = "white";
+}
+
+class Dog extends Super {
+    String color = "black";
+
+    void printColor() {
+        System.out.println(color);// prints color of Dog class
+        System.out.println(super.color);// prints color of Animal class
+    }
+}
+
+class TestSuper1 {
+    public static void main(String args[]) {
+        Dog d = new Dog();
+        d.printColor();
+    }
+}
