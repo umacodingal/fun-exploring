8b34bfba5d21bfe91f9185119552e536183450ee
umacodingal <uma.codingal@gmail.com>
2024-09-15 07:06:35 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,13 @@
+public class exception {
+    public class Main {
+        public static void main(String[] args) {
+            try {
+                int[] myNumbers = { 1, 2, 3 };
+                System.out.println(myNumbers[10]);
+            } catch (Exception e) {
+                System.out.println("Something went wrong.");
+            }
+        }
+    }
+    
+}
